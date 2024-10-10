137476f98dab9ab788f982b77ce22fb91bd7321c
Joao-Inacio <joaoinacio206@protonmail.com>
2024-10-10 11:05:40 -0300

    scripit para roda o app

--- app.py
+++ app.py
@@ -1,37 +1,17 @@
-from flask import Flask, render_template, request, redirect
-import sqlite3
-import sys
 import os
+import sys
+
+from flask import Flask
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from forms import DespesaForm 
-
+from routes.consulta import consulta_route
+from routes.home import home_route
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'minha_chave_secreta'  # Necessário para WTForms
 
-# Conexão simples com o banco de dados SQLite
-def conecta_bd():
-    return sqlite3.connect('data/balanco.db')
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = DespesaForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        # Pegando os dados do formulário e inserindo no banco
-        conn = conecta_bd()
-        cursor = conn.cursor()
-        cursor.execute(
-            'INSERT INTO despesas (ano, mes, dia, tipo, descricao, valor, prioridade) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            (
-                form.ano.data, form.mes.data, form.dia.data, form.tipo.data,
-                form.descricao.data, form.valor.data, form.prioridade.data
-            )
-        )
-        conn.commit()
-        conn.close()
-        return redirect('/')
-    return render_template('index.html', form=form)
+app.register_blueprint(home_route)
+app.register_blueprint(consulta_route)
 
 if __name__ == '__main__':
     app.run(debug=True)
